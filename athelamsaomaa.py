fda5e35e8f7b686f085bb22a6578064cc9dcacfc
tranquocdien <tranquocdien2002@gmail.com>
2020-12-20 15:53:44 -0800

    Hoan` thien.

--- athelamsaomaa.py
+++ athelamsaomaa.py
@@ -5,7 +5,7 @@ def main():
     emails = ("helloworld@Coderisme.com", "tranquocdien2002@gmail.com", "localspo@gmail.com")
     for email in emails:
         email_username, email_domain = emailProcess(email)
-    printemail(email_username, email_domain)
+        printemail(email_username, email_domain)
 
 
 if __name__ == "__main__":
