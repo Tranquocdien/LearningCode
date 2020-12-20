2f31faf66a98f5ff6f6d39e368fb505cb21a50c3
tranquocdien <tranquocdien2002@gmail.com>
2020-12-20 09:45:07 -0800

    Lenh if __name__ == __main__: main()

---
+++ athelamsaomaa.py
@@ -0,0 +1,13 @@
+from athea import emailProcess, printemail
+
+
+def main():
+    emails = ("helloworld@Coderisme.com", "tranquocdien2002@gmail.com", "localspo@gmail.com")
+    for email in emails:
+        email_username, email_domain = emailProcess(email)
+    printemail(email_username, email_domain)
+
+
+if __name__ == "__main__":
+    main()
+
